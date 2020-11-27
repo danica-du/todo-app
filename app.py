4e9b723fb64cabf8c3a1b43d70bd96df92fbbab3
danica-du <ddu@hmc.edu>
2020-11-26 21:02:48 -0800

    done w hello worlding

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)  # create an app instance
 def hello():
     return "Hello World!"
 
+# at the end point /<name>, call hello_name(name)
+@app.route("/<name>")
+def hello_name(name):
+    return "Hello " + name
+
 # on running python app.py, run the flask app
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
